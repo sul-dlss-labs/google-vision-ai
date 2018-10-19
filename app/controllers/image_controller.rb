c92bfb51255deee5220260bd88613e0d08756bad
Peter Mangiafico <peter@mangiafico.org>
2018-10-19 15:41:46 -0700

    add ability to submit images to google vision api

--- app/controllers/image_controller.rb
+++ app/controllers/image_controller.rb
@@ -4,4 +4,49 @@ class ImageController < ApplicationController
 
   end
 
+  def results
+
+    input_value = params[:input_value].strip.downcase
+
+    druid_match = /[a-zA-Z]{2}[0-9]{3}[a-zA-Z]{2}[0-9]{4}/.match(input_value)
+
+    if druid_match.nil?
+
+      @error = "The value entered was not a valid druid or PURL"
+      return false
+
+    end
+
+    @druid = druid_match.to_s
+
+    project_id = "sul-ai-studio" # Your Google Cloud Platform project ID
+
+    iiif_manifest_url = "https://purl.stanford.edu/#{@druid}/iiif/manifest"
+
+    begin
+
+      client = IiifGoogleCv::Client.new(manifest_url: iiif_manifest_url)
+      images = client.image_resources
+
+      @results = []
+      vision = Google::Cloud::Vision.new project: project_id
+      images.each do |image|
+        response = vision.image(image)
+        labels = response.labels.map { |label| {description: label.description, score: label.score} }
+        entities = response.web.entities.map { |entity| {description: entity.description, score: entity.score} }
+        @results << {image: image, labels: labels, entities: entities}
+      end
+
+    rescue Google::Cloud::InvalidArgumentError => e
+
+      @error = "Google returned an error: \"#{e.message}\".  Its likely this image is restricted in some way (either not viewable at all or only as a thumbnail)"
+
+    rescue StandardError => e
+
+      @error = "Something bad happened and we don't know what. This might help: \"#{e.message}\"."
+
+    end
+
+  end
+
 end
