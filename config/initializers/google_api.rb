05bee4a5fdbe37e38c3f158fedc303b98b145822
Peter Mangiafico <peter@mangiafico.org>
2018-10-29 14:18:50 -0700

    add google api initialization auth

---
+++ config/initializers/google_api.rb
@@ -0,0 +1,6 @@
+if Rails.env == "production"
+  ENV['GOOGLE_APPLICATION_CREDENTIALS'] = './google_application_credentials.json'
+  File.open("./google_application_credentials.json", "w+") do |f|
+    f.write(ERB.new(File.read('./google_application_credentials.json.tmp')).result)
+  end
+end
